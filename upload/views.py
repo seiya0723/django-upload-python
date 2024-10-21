16ead54cb6e0c84b9d0c661d98ff5c8f020300ad
Seiya Asahina <seiya@asahina>
2024-10-21 13:33:45 +0900

    add

--- upload/views.py
+++ upload/views.py
@@ -7,6 +7,11 @@ from .forms import DocumentForm
 from django.conf import settings
 import subprocess
 
+import importlib.util
+import os
+
+
+
 class IndexView(View):
     def get(self, request, *args, **kwargs):
 
@@ -22,10 +27,28 @@ class IndexView(View):
 
         # 仮想環境のファイルパスを作成(pythonファイルのある場所)
         venv_path       = str(settings.BASE_DIR) + "/venv/bin/"
+
+
+        #== subprocess モジュールで実行する==============
+
         
         # 実行結果を出力する。(仮想環境を有効にする)
         result  = subprocess.run(["python", program_path], capture_output=True,text=True , cwd=venv_path)
         print(result.stdout)
+
+
+        #===動的にimportして実行する==============
+
+        # ファイル名からモジュール名を生成
+        module_name = os.path.splitext(os.path.basename(program_path))[0]
+
+        # モジュール仕様を作成し、動的にインポート
+        spec        = importlib.util.spec_from_file_location(module_name, program_path)
+        module      = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+
+        module.hello()
+
 
         return render(request,"upload/index.html",context)
 
